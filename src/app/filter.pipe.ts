b7b23b9f78aeddca672ce05612fe7a241e99ce4a
Nyki <nykilundgren127@gmail.com>
2020-01-22 20:39:43 -0500

    fixed filter

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(todos: any, term: any[]): any {
-    if(term === undefined) return todos;
+  transform(todos: any, term: string): any {
+    if(!term) return todos;
       
     return todos.filter(function(todo: any){
       return todo.title.toLowerCase().includes(term.toLowerCase());
